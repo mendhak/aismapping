dd69f7dbe66db027957cac015d7cef2b7c9311ca
Mendhak <smaltaf@msn.com>
2013-03-16 20:26:19 +0000

    Raw url for nmea data

--- aismap/views.py
+++ aismap/views.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import ais
@@ -13,6 +14,11 @@ def home(request):
     resp.write(ais.decode('15PIIv7P00D5i9HNn2Q3G?wB0t0I', 0))
     return resp
 
+def rawdata(request):
+    resp = HttpResponse()
 
+
+    resp.write(file(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nmea-sample.txt")).read())
+    return resp
 
     #return render_to_response("map.html")
--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'aismap.views.home', name='home'),
+    url(r'^raw$', 'aismap.views.rawdata', name='home'),
     # url(r'^aismapping/', include('aismapping.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
