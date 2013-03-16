4fd0c0c5f0255a2384182f4ef026571e46703648
Mendhak <smaltaf@msn.com>
2013-03-16 23:10:32 +0000

    JS to get map bounds every few seconds

--- aismap/views.py
+++ aismap/views.py
@@ -12,8 +12,6 @@ def home(request):
     html = "<html><body>It is now %s.</body></html>" % now
     resp = HttpResponse()
     return render_to_response("map.html")
-    #resp.write(ais.decode('15PIIv7P00D5i9HNn2Q3G?wB0t0I', 0))
-    #return resp
 
 def rawdata(request):
     resp = HttpResponse()
@@ -25,29 +23,21 @@ def rawdata(request):
             m = re.search('!AIVDM,[\w]?,[\w]?,[\w]?,[\w]?,(?P<ais>[^,]*)', line)
             if m:
                 aisData = m.group('ais')
-                #resp.write("<br /><br />")
                 try:
                     aiss.append(ais.decode(aisData,0))
                     mmsis.append(ais.decode(aisData,0).get('mmsi'))
                 except:
                     pass
                     #resp.write("Could not decode " + aisData)
-
-        #resp.write("Total: " + str(len(mmsis)))
         resp.write("<br/>")
         repeated = ([x for x, y in collections.Counter(mmsis).items() if y > 2])
         for mm in repeated:
             for rm in (item for item in aiss if item["mmsi"] == mm):
                 resp.write(str(rm.get('mmsi')) + " at " + str(rm.get('timestamp')) + " at " + str(rm.get('x')) + "," + str(rm.get('y')))
                 resp.write("<br/>")
-            #resp.write(repeatingMmsi.get('mmsi'))
-            #resp.write(mm)
-
 
     #resp.write(file(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nmea-sample.txt")).read())
     return resp
-
-    #return render_to_response("map.html")
 
 
 def storedata(request):
@@ -80,16 +70,5 @@ def storedata(request):
                                         + ");")
                     except Exception as e:
                         print e.message
-
-            #repeated = ([x for x, y in collections.Counter(mmsis).items() if y > 2])
-            #for mm in repeated:
-            #    for rm in (item for item in aiss if item["mmsi"] == mm):
-                    #resp.write(str(rm.get('mmsi')) + " at " + str(rm.get('timestamp')) + " at " + str(rm.get('x')) + "," + str(rm.get('y')))
-                    #resp.write("<br/>")
-                #resp.write(repeatingMmsi.get('mmsi'))
-                #resp.write(mm)
-
-
-        #resp.write(file(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nmea-sample.txt")).read())
 
     return HttpResponse("Done")
