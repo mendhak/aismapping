f30b974f3808203c334b94ae533b5e22a4857751
Mendhak <smaltaf@msn.com>
2013-03-17 08:44:50 +0000

    Red circle for aground/anchored/moored vehicles

--- aismap/views.py
+++ aismap/views.py
@@ -14,14 +14,15 @@ def home(request):
     resp = HttpResponse()
     return render_to_response("map.html")
 
+
 def getVisibleShips(request):
     toprightx = request.GET.get('toprightx')
     toprighty = request.GET.get('toprighty')
     bottomleftx = request.GET.get('bottomleftx')
     bottomlefty = request.GET.get('bottomlefty')
 
-# { "mmsi":"12341234","x":"1234","y":"234" },
-#     { "mmsi":"12341234","x":"1234","y":"234" },
+    # { "mmsi":"12341234","x":"1234","y":"234" },
+    #     { "mmsi":"12341234","x":"1234","y":"234" },
 
     jsonArray = "{\"ships\": [ "
 
@@ -30,15 +31,16 @@ def getVisibleShips(request):
     with conn:
         cur = conn.cursor()
         cur.execute("SELECT * FROM AIVDM WHERE x > {0} AND x < {1} AND y > {2} ANd y < {3} LIMIT 20"
-                    .format(bottomleftx, toprightx, bottomlefty, toprighty))
+        .format(bottomleftx, toprightx, bottomlefty, toprighty))
         rows = cur.fetchall()
 
         for row in rows:
-            jsonArray += "{{ \"mmsi\":\"{0}\",\"x\":\"{1}\",\"y\":\"{2}\",\"heading\":\"{3}\" }},".format(row[0],row[1],row[2],row[4])
+            jsonArray += "{{ \"mmsi\":\"{0}\",\"x\":\"{1}\",\"y\":\"{2}\",\"heading\":\"{3}\",\"navstatus\":\"{4}\" }},".format(
+                row[0], row[1], row[2], row[4], row[3])
 
     jsonArray = jsonArray[:-1] + "    ]}"
 
-    return HttpResponse(jsonArray,mimetype="application/json")
+    return HttpResponse(jsonArray, mimetype="application/json")
 
 
 def rawdata(request):
@@ -46,14 +48,14 @@ def rawdata(request):
     mmsis = []
     aiss = []
 
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nmea-sample.txt"),'r') as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "nmea-sample.txt"), 'r') as f:
         for line in f:
             m = re.search('!AIVDM,[\w]?,[\w]?,[\w]?,[\w]?,(?P<ais>[^,]*)', line)
             if m:
                 aisData = m.group('ais')
                 try:
-                    aiss.append(ais.decode(aisData,0))
-                    mmsis.append(ais.decode(aisData,0).get('mmsi'))
+                    aiss.append(ais.decode(aisData, 0))
+                    mmsis.append(ais.decode(aisData, 0).get('mmsi'))
                 except:
                     pass
                     #resp.write("Could not decode " + aisData)
@@ -61,7 +63,9 @@ def rawdata(request):
         repeated = ([x for x, y in collections.Counter(mmsis).items() if y > 2])
         for mm in repeated:
             for rm in (item for item in aiss if item["mmsi"] == mm):
-                resp.write(str(rm.get('mmsi')) + " at " + str(rm.get('timestamp')) + " at " + str(rm.get('x')) + "," + str(rm.get('y')))
+                resp.write(
+                    str(rm.get('mmsi')) + " at " + str(rm.get('timestamp')) + " at " + str(rm.get('x')) + "," + str(
+                        rm.get('y')))
                 resp.write("<br/>")
 
     #resp.write(file(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nmea-sample.txt")).read())
@@ -69,23 +73,23 @@ def rawdata(request):
 
 
 def storedata(request):
-
     conn = sqlite3.connect(r"ais.db")
 
     with conn:
 
         cur = conn.cursor()
         cur.execute("DROP TABLE IF EXISTS AIVDM")
-        cur.execute("CREATE TABLE IF NOT EXISTS AIVDM(mmsi INT, x DEC, y DEC, nav_status INT, true_heading INT, timestamp INT)")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS AIVDM(mmsi INT, x DEC, y DEC, nav_status INT, true_heading INT, timestamp INT)")
 
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"nmea-sample.txt"),'r') as f:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "nmea-sample.txt"), 'r') as f:
             for line in f:
                 m = re.search('!AIVDM,[\w]?,[\w]?,[\w]?,[\w]?,(?P<ais>[^,]*)', line)
                 if m:
                     aisData = m.group('ais')
                     #resp.write("<br /><br />")
                     try:
-                        aisDecoded = ais.decode(aisData,0)
+                        aisDecoded = ais.decode(aisData, 0)
 
                         if aisDecoded:
                             if aisDecoded.get('mmsi') <= 0 or aisDecoded.get('x') > 180 or aisDecoded.get('y') > 90:
@@ -93,7 +97,8 @@ def storedata(request):
 
                             cur.execute("INSERT INTO AIVDM VALUES(" + str(aisDecoded.get('mmsi')) + ","
                                         + str(aisDecoded.get('x')) + "," + str(aisDecoded.get('y')) + ","
-                                        + str(aisDecoded.get('nav_status')) + "," + str(aisDecoded.get('true_heading')) + ","
+                                        + str(aisDecoded.get('nav_status')) + "," + str(
+                                aisDecoded.get('true_heading')) + ","
                                         + str(aisDecoded.get('timestamp'))
                                         + ");")
                     except Exception as e:
